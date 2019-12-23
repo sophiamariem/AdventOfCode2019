6c3c78cf64eefad7266a7c77f2cd5ac70af04235
Sophia <sophiamariem@hotmail.com>
2019-12-23 00:50:15 +0000

    Day 5 - part 2

--- src/main/kotlin/e_sunny_with_a_chance_of_asteroids/Test2.kt
+++ src/main/kotlin/e_sunny_with_a_chance_of_asteroids/Test2.kt
@@ -1,26 +1,8 @@
 package main.kotlin.e_sunny_with_a_chance_of_asteroids
 
-import java.io.File
+const val instruction2 = 5
 
-const val FILENAME = "src/main/kotlin/e_sunny_with_a_chance_of_asteroids/input.txt"
-const val instruction = 1
-
-fun getList(): ArrayList<Int> {
-    var input = ""
-    File(FILENAME).forEachLine { input = it }
-    var l = input.split(",").toList().map { it.toInt() }.toList()
-    return ArrayList(l)
-}
-
-fun getCode(code: Int): ArrayList<Int> {
-    var params: ArrayList<Int> = ArrayList(3)
-    params.add((code / 100) % 10)
-    params.add(code / 1000)
-    params.add(0) // always position
-    return params
-}
-
-fun instruction(): Int {
+fun instruction2(): Int {
     var instructions = getList()
     var s = 0
     var output = -1
@@ -51,15 +33,36 @@ fun instruction(): Int {
                 s += 4
             }
             3 -> {
-                instructions[instructions[indx + 1]] = instruction
+                instructions[instructions[indx + 1]] = instruction2
                 s += 2
             }
             4 -> {
                 output = getValue(instructions, instructions[indx + 1], 0, code)
                 s += 2
             }
+            5 -> {
+                if(getValue(instructions, instructions[indx + 1], 0, code) != 0) {
+                    s = getValue(instructions, instructions[indx + 2], 1, code)
+                } else s += 3
+            }
+            6 -> {
+                if(getValue(instructions, instructions[indx + 1], 0, code) == 0) {
+                    s = getValue(instructions, instructions[indx + 2], 1, code)
+                } else s += 3
+            }
+            7 -> {
+                instructions[instructions[indx + 3]] =
+                    if (getValue(instructions, instructions[indx + 1], 0, code) <
+                        getValue(instructions, instructions[indx + 2], 1, code)) 1 else 0
+                s += 4
+            }
+            8 -> {
+                instructions[instructions[indx + 3]] =
+                    if (getValue(instructions, instructions[indx + 1], 0, code) ==
+                        getValue(instructions, instructions[indx + 2], 1, code)) 1 else 0
+                s += 4
+            }
             else -> {
-                output = -1
                 s += instructions.size
             }
         }
@@ -69,14 +72,6 @@ fun instruction(): Int {
     return output
 }
 
-fun getValue(instructions: ArrayList<Int>, value: Int, index: Int, code: ArrayList<Int>): Int {
-    return when (code[index]) {
-        0 -> instructions[value]
-        1 -> value
-        else -> 0
-    }
-}
-
 fun main() {
-    println("Part_1: ${instruction()}")
+    println("Part_2: ${instruction2()}")
 }
